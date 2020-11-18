c9d2105bb2ec86cf1809001792023daf3eda46c7
JohnnyUtah-9 <sidney.b.johnson3@gmail.com>
2020-11-18 15:30:25 -0500

    Task: updated Dockerfile path

--- src/Calculator.py
+++ src/Calculator.py
@@ -9,29 +9,30 @@ class Calculator:
     result = 0
 
     def __init__(self):
+        self.result = 1
         pass
 
     def add(self, a, b):
-        self.result = Addition(a, b)
+        self.result = Addition.addition(a, b)
         return self.result
 
     def subtract(self, a, b):
-        self.result = Subtraction(a, b)
+        self.result = Subtraction.subtraction(a, b)
         return self.result
 
     def multiply(self, a, b):
-        self.result = Multiplication(a, b)
+        self.result = Multiplication.multiplication(a, b)
         return self.result
 
     def divide(self, a, b):
-        self.result = Division(a, b)
+        self.result = Division.division(a, b)
         return self.result
 
-    def sqr(self, a):
-        self.result = Square(a)
+    def square(self, a):
+        self.result = Square.square(a)
         return self.result
 
-    def sqrt(self, a, b):
-        self.result = Square_Root(a, b)
+    def square_root(self, a, b):
+        self.result = Square_Root.sqrt(a, b)
         return self.result
 
